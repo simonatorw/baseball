36ac8589b063adeaee8cad4f9f9b5ea1160f87a8
simonbigredtiger <simon@bigredtiger.com>
2017-08-13 01:52:45 -0400

    Added team config, players status

---
+++ teams.js
@@ -0,0 +1,71 @@
+const nny = {
+	team: 'NYY',
+	longName: 'New York Yankees',
+	color: 'black',
+	starters: [
+		{
+			name: 'Mariano Rivera'
+		},
+		{
+			name: 'Robinson Cano'
+		},
+		{
+			name: 'Johnny Damon'
+		},
+		{
+			name: 'Derek Jeter'
+		},
+		{
+			name: 'Hideki Matsui'
+		},
+		{
+			name: 'Jorge Posada'
+		},
+		{
+			name: 'Cody Ransom'
+		},
+		{
+			name: 'Nick Swisher'
+		},
+		{
+			name: 'Mark Teixeira'
+		}
+	],
+	pitcherIndex: 0
+};
+
+const sf = {
+	team: 'SF',
+	longName: 'San Francisco Giants',
+	color: 'orange',
+	starters: [
+		{
+			name: 'Tim Lincecum' 
+		},
+		{
+			name: 'Buster Posey'
+		},
+		{
+			name: 'Brandon Belt'
+		},
+		{		
+			name: 'Joe Panik'
+		},
+		{
+			name: 'Pablo Sandoval'
+		},
+		{
+			name: 'Brandon Crawford'
+		},
+		{
+			name: 'Mike Morse'
+		},
+		{
+			name: 'Angel Pagan'
+		},
+		{
+			name: 'Hunter Pence'
+		}
+	],
+	pitcherIndex: 0
+};
