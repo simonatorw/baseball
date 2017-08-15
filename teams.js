109e47059a86d1b05d0e02f5cab6834490d554cb
simonbigredtiger <simon@bigredtiger.com>
2017-08-14 23:41:45 -0400

    Added field array

--- teams.js
+++ teams.js
@@ -5,39 +5,48 @@ const nny = {
 	starters: [
 		{
 			name: 'Mariano Rivera',
-			ba: .2
+			ba: .2,
+			hr: 10
 		},
 		{
 			name: 'Robinson Cano',
-			ba: .305
+			ba: .305,
+			hr: 297
 		},
 		{
 			name: 'Johnny Damon',
-			ba: .284
+			ba: .284,
+			hr: 235
 		},
 		{
 			name: 'Derek Jeter',
-			ba: .310
+			ba: .310,
+			hr: 260
 		},
 		{
 			name: 'Hideki Matsui',
-			ba: .282
+			ba: .282,
+			hr: 175
 		},
 		{
 			name: 'Jorge Posada',
-			ba: .273
+			ba: .273,
+			hr: 275
 		},
 		{
 			name: 'Cody Ransom',
-			ba: .213
+			ba: .213,
+			hr: 30
 		},
 		{
 			name: 'Nick Swisher',
-			ba: .249
+			ba: .249,
+			hr: 245
 		},
 		{
 			name: 'Mark Teixeira',
-			ba: .268
+			ba: .268,
+			hr: 409
 		}
 	],
 	pitcherIndex: 0
@@ -50,39 +59,48 @@ const sf = {
 	starters: [
 		{
 			name: 'Tim Lincecum',
-			ba: .2
+			ba: .2,
+			hr: 10
 		},
 		{
 			name: 'Buster Posey',
-			ba: .308
+			ba: .308,
+			hr: 128
 		},
 		{
 			name: 'Brandon Belt',
-			ba: .268
+			ba: .268,
+			hr: 98
 		},
 		{		
 			name: 'Joe Panik',
-			ba: .276
+			ba: .276,
+			hr: 26
 		},
 		{
 			name: 'Pablo Sandoval',
-			ba: .286
+			ba: .286,
+			hr: 121
 		},
 		{
 			name: 'Brandon Crawford',
-			ba: .249
+			ba: .249,
+			hr: 68
 		},
 		{
 			name: 'Mike Morse',
-			ba: .274
+			ba: .274,
+			hr: 105
 		},
 		{
 			name: 'Angel Pagan',
-			ba: .28
+			ba: .28,
+			hr: 64
 		},
 		{
 			name: 'Hunter Pence',
-			ba: .282
+			ba: .282,
+			hr: 217
 		}
 	],
 	pitcherIndex: 0
